0379559cf606e6c26a8fa7379864255f829e46e3
PatricioOyarzo <patricio.oyarzo@usm.cl>
2025-06-05 19:53:25 -0400

    sacar cambios

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Travelin',
+      title: 'App0',
       
       routes: {
         '/': (context) => BottomNav(),
